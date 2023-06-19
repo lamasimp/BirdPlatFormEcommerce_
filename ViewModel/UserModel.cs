efc7d974d41cbcfb24fd1c93848f3db278782a0c
longnhatlekk <135359437+longnhatlekk@users.noreply.github.com>
2023-06-19 13:41:21 +0700

    updateuser

---
+++ ViewModel/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BirdPlatFormEcommerce.ViewModel
+{
+    public class UserModel
+    {
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Name { get; set; }
+    }
+}
