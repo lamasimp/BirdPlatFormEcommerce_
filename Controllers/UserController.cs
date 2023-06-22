db550f2267c26366232fa90dc3925c76fd5e6c18
DuongHienne <thaohien1372002@gmail.com>
2023-06-23 01:09:37 +0700

    api

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -94,7 +94,9 @@ namespace BirdPlatForm.Controllers
                     new Claim("Username", user.Name.ToString()),
                   new Claim("Dob", user.Dob.ToString()),
                     new Claim(ClaimTypes.Role, user.RoleId),
-                   
+                    new Claim("IsShop",user.IsShop.ToString()!),
+                    new Claim("Avatar",user.Avatar),
+
 
 
                 }),
