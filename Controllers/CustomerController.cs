9ae09b3517b7512c25f493e0d950eaab53cedfc0
longnhatlekk <135359437+longnhatlekk@users.noreply.github.com>
2023-06-23 22:20:35 +0700

    gethighmoney

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,61 +1,66 @@
-﻿//using BirdPlatFormEcommerce.Entity;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
+﻿using BirdPlatFormEcommerce.Entity;
+using BirdPlatFormEcommerce.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-//namespace BirdPlatFormEcommerce.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class CustomerController : ControllerBase
-//    {
-//        private readonly SwpContext _context;
+namespace BirdPlatFormEcommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly SwpContext _context;
 
-//        public CustomerController(SwpContext swp) 
-//        {
-//            _context = swp;        
-//        }
-//        [HttpPost("OderProduct")]
-//        public async Task<IActionResult> OderProduct(TbOrder oder)
-//        {
-//            var useridClaim = User.Claims.FirstOrDefault(u => u.Type == "UserId");
-//            if(useridClaim == null)
-//            {
-//                throw new Exception("Please Login");
-//            }
-//            int userid = int.Parse(useridClaim.Value);
-//            //kiem tra xem null hay khong
-//            if(oder == null || oder.TbOrderDetails == null || oder.TbOrderDetails.Count == 0)
-//            {
-//                return  BadRequest("Invalid order data");
-//            }
-//            //kiem tra thong tin va ngay mua hang 
-//            oder.UserId = userid;
-//            oder.OrderDate = DateTime.Now;
+        public CustomerController(SwpContext swp)
+        {
+            _context = swp;
+        }
 
-//            //tinh tien mua hang 
-//            foreach(var oderDetail in oder.TbOrderDetails)
-//            {
-//                var product = await _context.TbProducts.FirstOrDefaultAsync(x => x.ProductId == oderDetail.ProductId);
-//                if(product == null)
-//                {
-//                    return BadRequest($"Invalid product id: {oderDetail.ProductId}");
-//                }
-//                oderDetail.SubTotal = product.Price * oderDetail.Quantity;
-//            }
-//            try {
-//                await _context.TbOrders.AddAsync(oder);
-//                await _context.SaveChangesAsync();
+        //[HttpPost("OderProduct")]
+        //public async Task<IActionResult> OderProduct(TbOrder oder)
+        //{
+        //    var useridClaim = User.Claims.FirstOrDefault(u => u.Type == "UserId");
+        //    if (useridClaim == null)
+        //    {
+        //        throw new Exception("Please Login");
+        //    }
+        //    int userid = int.Parse(useridClaim.Value);
+        //    //kiem tra xem null hay khong
+        //    if (oder == null || oder.TbOrderDetails == null || oder.TbOrderDetails.Count == 0)
+        //    {
+        //        return BadRequest("Invalid order data");
+        //    }
+        //    //kiem tra thong tin va ngay mua hang 
+        //    oder.UserId = userid;
+        //    oder.OrderDate = DateTime.Now;
 
-//            } catch (Exception e)
-//            {
-//                return StatusCode(500, $"Error creating order: {e.Message}");
+        //    //tinh tien mua hang 
+        //    foreach (var oderDetail in oder.TbOrderDetails)
+        //    {
+        //        var product = await _context.TbProducts.FirstOrDefaultAsync(x => x.ProductId == oderDetail.ProductId);
+        //        if (product == null)
+        //        {
+        //            return BadRequest($"Invalid product id: {oderDetail.ProductId}");
+        //        }
+        //        oderDetail.SubTotal = product.Price * oderDetail.Quantity;
+        //    }
+        //    try
+        //    {
+        //        await _context.TbOrders.AddAsync(oder);
+        //        await _context.SaveChangesAsync();
 
-//            }
-//            return Ok("Success");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return StatusCode(500, $"Error creating order: {e.Message}");
 
-//            }
+        //    }
+        //    return Ok("Success");
+
+        //}
+       
 
 
-//    }
-//}
+    }
+}
---
+++ ViewModel/ShoptotalAmount.cs
@@ -0,0 +1,8 @@
+﻿namespace BirdPlatFormEcommerce.ViewModel
+{
+    public class ShoptotalAmount
+    {
+        public int shopId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
