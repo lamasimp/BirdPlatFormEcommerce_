032752a34a753280e4d537ecacc7f40ae94a4838
HoangChiDuong <duonghoangasd@gmail.com>
2023-07-03 09:29:20 +0700

    Update0703

--- Order/IOrderService.cs
+++ Order/IOrderService.cs
@@ -9,15 +9,16 @@ namespace BirdPlatFormEcommerce.Order
         public Task<List<TbOrder>> CreateOrder(int userId, CreateOrderModel orderModel);
 
         public Task<TbOrder?> GetOrder(int orderId);
+        public Task<List<TbOrder>> GetOrders(List<int> orderIds);
+        public Task<List<TbOrder>> GetOrderByPaymentId(int paymentId);
 
-        public Task<string?> PayOrder(TbOrder order, PaymentMethod method);
 
         public Task<TbOrder> UpdateOrder(TbOrder order);
-
-        public Task<TbOrder?> GetOrderByPaymentId(int paymentId);
-        public Task<TbOrder> CompleteOrder(TbOrder order);
+        public Task<List<TbOrder>> CompleteOrder(List<int> processedOrderIds);
         public Task<List<TbOrder>> GetConfirmedOrdersByUser(int userId, int toConfirm);
         public Task<List<TbOrderDetail>> Orderservice(int shopid);
-        public Task<List<TbOrder>> GetConfirmedOrdersByShop(int userId,int shopId);
+        public Task<string?> PayOrders(List<int> processedOrderIds, PaymentMethod method);
+
+        public Task<List<TbOrder>> GetConfirmedOrdersByShop(int userId, int shopId);
     }
 }
