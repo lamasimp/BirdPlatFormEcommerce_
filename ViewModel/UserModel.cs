42211ec0dd115e59319fc09e9fd44bc5e59438e8
Lê Long Nhật <longnhatlekk@gmail.com>
2023-07-06 14:44:09 +0700

    detailcus

--- ViewModel/UserModel.cs
+++ ViewModel/UserModel.cs
@@ -2,11 +2,25 @@
 {
     public class UserModel
     {
-         public DateTime? Dob { get; set; }
+        public DateTime? Dob { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Gender { get; set; }
         public string Name { get; set; }
         public IFormFile avatar { get; set; }
     }
+    public class Customer
+        {
+
+       public DateTime? birth { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Avatar { get; set; }
+
+        }
+
 }
