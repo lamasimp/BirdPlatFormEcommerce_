1de5980d2a6556432a520472409e7b5d0ac8bab4
HoangChiDuong <duonghoangasd@gmail.com>
2023-07-08 23:44:21 +0700

    update08/07Order2

--- NEntity/TbFeedbackImage.cs
+++ NEntity/TbFeedbackImage.cs
@@ -7,11 +7,11 @@ public partial class TbFeedbackImage
 {
     public int FbImgId { get; set; }
 
-    public int? FeedbackId { get; set; }
+    public int FeedbackId { get; set; }
 
-    public string? ImagePath { get; set; }
+    public string ImagePath { get; set; }
 
-    public long? FileSize { get; set; }
+    public long FileSize { get; set; }
 
     public virtual TbFeedback? Feedback { get; set; }
 }
