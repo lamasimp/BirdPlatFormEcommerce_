e959c2a63eedbbae9455e0872d1b3ac076c9ef94
Lê Long Nhật <longnhatlekk@gmail.com>
2023-07-28 03:49:35 +0700

    update2807

--- NEntity/SwpDataBaseContext.cs
+++ NEntity/SwpDataBaseContext.cs
@@ -54,7 +54,7 @@ public partial class SwpDataBaseContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-JUMV5BN\\SQLEXPRESS2012;Initial Catalog=swppp;Integrated Security=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-I2GP951T\\LONGNHAT;Initial Catalog=SWP301Database;Integrated Security=True;TrustServerCertificate=True");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TbAddressReceive>(entity =>
